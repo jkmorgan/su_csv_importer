0e72b8ce69877b9005603f178497b0cfb20c4a78
Kunteper <can158@gmail.com>
2013-08-26 18:29:22 -0400

    update

--- csv_import.php
+++ csv_import.php
@@ -47,22 +47,14 @@ function csv_import_form_page(){
 
 
 	<?php
-	//reader_handler();
-	/*$file_path = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'asdasd.csv';
-	$rawfile = file_get_contents($file_path);
-	$file = str_getcsv($rawfile);
-	for ($i = 0; $i <= count($file); $i++){
-		echo '<p>' . $file[$i] .'  '. $i . '</p>';
-	}*/
-
 }
 //      0           1              2             3              4                   5                          6                             7                                 8                    9
 // ['article', 'article_name', 'sections', academics', 'classifications', 'geo-spatial coverages', 'historical coverages', 'research sample characteristics', 'types, methods, approaches', 'contributors']
 function csv_handler(){ 
-	$csv_file = $_REQUEST['file'];
-	//$decoded_csv = csv_decoder($csv_file);
-	$decoded_csv = str_getcsv($csv_file, '	');
-	$ldiv = 11;
+	$seperator = '	'; //csv value seperator. tab space in this case.
+	$csv_file = str_replace("\n", $seperator, $_REQUEST['file']);
+	$decoded_csv = str_getcsv($csv_file, $seperator);
+	$ldiv = 29; // number of columns.
 	$articles = array(); // articles[0] == index
 
 	//arrange the posts in array
@@ -85,30 +77,31 @@ function csv_handler(){
 	for ($i = 1; $i < count($articles); $i++){
 		$selected_article = $articles[$i];
 		$post = array(
-			'post_content' => $selected_article[0],
-			'post_title' => $selected_article[1],
+			'post_content' => $selected_article[8][0],
+			'post_title' => $selected_article[9][0],
 			//'post_status' => 'inherit',
 			'post_date' => date_time(),
 			'post_date_gmt' => date_time(),
 			'post_type' => 'journal_article',
+			'tags_input' => $selected_article[17],
 			'tax_input' => array(
-				'journal_sections' => term_inserter($selected_article[2], 'journal_sections'),//$selected_article[2],  //todo butun termleri id lerine cevir;
-				'journal_academics' => term_inserter($selected_article[3], 'journal_academics'),
+				'journal_sections' => term_inserter($selected_article[11], 'journal_sections'),
+				/*'journal_academics' => term_inserter($selected_article[3], 'journal_academics'),
 				'journal_classification' => term_inserter($selected_article[4], 'journal_classification'),
 				'journal_geospatial_coverage' => term_inserter($selected_article[5], 'journal_geospatial_coverage'),
 				'journal_historical_coverage' => term_inserter($selected_article[6], 'journal_historical_coverage'),
 				'journal_research_sample' => term_inserter($selected_article[7], 'journal_research_sample'),
 				'journal_type_method_approach' => term_inserter($selected_article[8], 'journal_type_method_approach'),
-				'journal_contributors' => term_inserter($selected_article[9], 'journal_contributors')
+				'journal_contributors' => term_inserter($selected_article[9], 'journal_contributors')*/
 				)
 
 			);
 
 		print_r(wp_insert_post($post));
-		//print_r(term_inserter(array('aaca4', 'aaca5', 'aaca6'), 'journal_academics'));
 		
+		print_r($post);	
 	}
-	print_r($articles);
+	//print_r($articles);
 	
 	exit();
 }
@@ -126,14 +119,6 @@ function date_time(){
 
 //
 function term_inserter($term, $taxonomy){
-	/*$termid = get_term_by( 'slug', $term, $taxonomy );
-	if ($termid == ''){
-		$termid = wp_insert_term($term, $taxonomy);
-		$termid = $termid['term_id'];
-		return $termid;
-	}else{
-		return $termid->term_id;
-	}*/
 	if (count($term) == 0){
 		return '';
 	}else{
@@ -142,9 +127,11 @@ function term_inserter($term, $taxonomy){
 			$selected_term = get_term_by( 'slug', $term[$i], $taxonomy );
 			if ($selected_term == ''){
 				$selected_term = wp_insert_term($term[$i], $taxonomy);
-				$selected_term = $selected_term['term_id'];
+				print_r($selected_term);
+				//$selected_term = $selected_term['term_id'];
 			}else{
 				$selected_term = $selected_term->term_id;
+				print_r($selected_term);
 			}
 			$term_array[] = $selected_term;			
 			
